f4ace943cf55b94b5f6c8fbd63b3a8727fafaaf2
Dênis Júnio <denisjunioag1991@gmail.com>
2020-08-19 15:36:40 -0300

    Internal changes to OpenProject (Pull Up)

--- src/integrations/openproject.js
+++ src/integrations/openproject.js
@@ -2,6 +2,45 @@
 clockifyButton.render('table.work-package-table tbody tr td.id:not(.clockify)', {observe: true}, (elem) => {
     var link,
         container = elem,
+        tagName = $('span[data-field-name="customField18"]', elem.parentNode).textContent.trim(),
+        description = '[' + tagName + '] ' + $('span[data-field-name="type"]', elem.parentNode).textContent.trim() +
+            ' #' + $('span[data-field-name="id"]', elem.parentNode).textContent.trim() +
+            ': ' + $('span[data-field-name="subject"]', elem.parentNode).textContent.trim(),
+        projectName = $('#projects-menu').title.trim();
+
+    if (tagName === '-') {
+        // alert('Tracking Type not Define!');
+    } else {
+        link = clockifyButton.createSmallButton(description, projectName);
+        container.appendChild(link);
+    }
+
+});
+
+// Work packages details view
+clockifyButton.render('.work-packages--show-view:not(.clockify)', {observe: true}, (elem) => {
+    var link,
+        container = $('.attributes-group--header', elem),
+        tagName = $('span[data-field-name="customField18"]', elem.parentNode).textContent.trim(),
+        description = '[' + tagName + '] ' + $('.type').textContent.trim() + ' ' +
+            $('div.work-packages--info-row span', elem.parentNode).textContent.trim() +
+            ': ' + $('.subject').textContent.trim(),
+        projectName = $('#projects-menu').title.trim();
+
+    if (tagName === '-') {
+        // alert('Tracking Type not Define!');
+    } else {
+        link = clockifyButton.createButton(description, projectName);
+        container.insertBefore(link, container.firstChild);
+    }
+
+});
+
+/**
+ // Work packages list items
+ clockifyButton.render('table.work-package-table tbody tr td.id:not(.clockify)', {observe: true}, (elem) => {
+    var link,
+        container = elem,
         description = $('span[data-field-name="subject"]', elem.parentNode).textContent.trim(),
         projectName = $('#projects-menu').title.trim();
 
@@ -10,8 +49,8 @@ clockifyButton.render('table.work-package-table tbody tr td.id:not(.clockify)', 
     container.appendChild(link);
 });
 
-// Work packages details view
-clockifyButton.render('.work-packages--show-view:not(.clockify)', {observe: true}, (elem) => {
+ // Work packages details view
+ clockifyButton.render('.work-packages--show-view:not(.clockify)', {observe: true}, (elem) => {
     var link,
         container = $('.attributes-group--header', elem),
         description = $('.subject').textContent.trim(),
@@ -21,3 +60,4 @@ clockifyButton.render('.work-packages--show-view:not(.clockify)', {observe: true
 
     container.insertBefore(link, container.firstChild);
 });
+ */
